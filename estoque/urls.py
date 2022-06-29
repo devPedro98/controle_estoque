b900a09f9845a00789e3142517e6f3e8f47e9da1
devPedro98 <pedroribeirobarrios@gmail.com>
2022-06-29 20:20:26 -0300

    estoque_entrada_form.html

--- estoque/urls.py
+++ estoque/urls.py
@@ -7,4 +7,5 @@ app_name = 'estoque'
 urlpatterns = [
     path('', v.estoque_entrada_list, name='url_estoque_entrada_list'),
     path('<int:pk>/', v.estoque_entrada_detail, name='url_estoque_entrada_detail'),
+    path('add/', v.estoque_entrada_add, name='url_estoque_entrada_add'),
 ]
