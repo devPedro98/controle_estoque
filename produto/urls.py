82923e21f3492dbd021320f82a4b30a7170318df
devPedro98 <pedroribeirobarrios@gmail.com>
2022-07-01 19:50:37 -0300

    Ajax para atualizar o saldo de cada produto

--- produto/urls.py
+++ produto/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:pk>/', v.produto_detail, name='url_produto_detail'),
     path('add/', v.ProdutoCreate.as_view(), name='url_produto_add'),
     path('<int:pk>/edit', v.ProdutoUpdate.as_view(), name='url_produto_edit'),
+    path('<int:pk>/json/', v.produto_json, name='url_produto_json'),
 ]
