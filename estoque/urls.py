795b9542aab48d460c5bfed6b983fa448c573726
devPedro98 <pedroribeirobarrios@gmail.com>
2022-07-05 18:47:23 -0300

    estoque_saida_list.html

--- estoque/urls.py
+++ estoque/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', v.estoque_entrada_list, name='url_estoque_entrada_list'),
     path('<int:pk>/', v.estoque_entrada_detail, name='url_estoque_entrada_detail'),
     path('add/', v.estoque_entrada_add, name='url_estoque_entrada_add'),
+    path('saida/', v.estoque_saida_list, name='url_estoque_saida_list'),
 ]
