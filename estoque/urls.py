006ccd35887c83d9753e07a12857baf9f1286836
devPedro98 <pedroribeirobarrios@gmail.com>
2022-07-07 08:08:59 -0300

    Refatorando: Transformando estoque_entrada_detail em CBV

--- estoque/urls.py
+++ estoque/urls.py
@@ -6,9 +6,9 @@ app_name = 'estoque'
 
 urlpatterns = [
     path('', v.EstoqueEntradaList.as_view(), name='url_estoque_entrada_list'),
-    path('<int:pk>/', v.estoque_entrada_detail, name='url_estoque_entrada_detail'), # noqa
+    path('<int:pk>/', v.EstoqueEntradaDetail.as_view(), name='url_estoque_entrada_detail'), # noqa
     path('add/', v.estoque_entrada_add, name='url_estoque_entrada_add'),
     path('saida/', v.EstoqueSaidaList.as_view(), name='url_estoque_saida_list'), # noqa
-    path('saida/<int:pk>/', v.estoque_saida_detail, name='url_estoque_saida_detail'), # noqa
+    path('saida/<int:pk>/', v.EstoqueSaidaDetail.as_view(), name='url_estoque_saida_detail'), # noqa
     path('saida/add/', v.estoque_saida_add, name='url_estoque_saida_add'),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,7 +1,7 @@
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from produto.models import Produto
 
 from .forms import EstoqueForm, EstoqueItensForm
@@ -31,6 +31,16 @@ def estoque_entrada_detail(request, pk):
     obj = EstoqueEntrada.objects.get(pk=pk)
     context = {'object': obj, 'url_list': 'estoque:url_estoque_entrada_list'}
     return render(request, template_name, context)
+
+
+class EstoqueEntradaDetail(DetailView):
+    model = EstoqueEntrada
+    template_name = 'estoque/estoque_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueEntradaDetail, self).get_context_data(**kwargs)
+        context['url_list'] = 'estoque:url_estoque_entrada_list'
+        return context
 
 
 def dar_baixa_estoque(form):
@@ -103,6 +113,16 @@ def estoque_saida_detail(request, pk):
     obj = EstoqueSaida.objects.get(pk=pk)
     context = {'object': obj, 'url_list': 'estoque:url_estoque_saida_list'}
     return render(request, template_name, context)
+
+
+class EstoqueSaidaDetail(DetailView):
+    model = EstoqueSaida
+    template_name = 'estoque/estoque_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueSaidaDetail, self).get_context_data(**kwargs)
+        context['url_list'] = 'estoque:url_estoque_saida_list'
+        return context
 
 
 def estoque_saida_add(request):
